85bb85f04417ecf962ae4ae000edf41852190e21
Jonny Cavell <jonny.cavell@gmail.com>
2020-09-16 13:01:36 +0100

    P4-2185
    Objective: import move/journey/event reporting data from JSON
    
    Add OneToMany annotation for mapping Events to a Move
    Failing test - pushing for more investigation.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/reporting/EventRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/reporting/EventRepository.kt
@@ -1,8 +1,16 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc.reporting
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import java.util.UUID
 
 interface EventRepository : CrudRepository<Event, UUID> {
+
+//    @Query("SELECT e FROM Event e WHERE e.eventableType = :eventableType and e.eventableId IN (:eventableIds)")
+//    fun findByEventableTypeAndEventableIds(
+//            @Param("eventableType") eventableType: String,
+//            @Param("eventableIds") eventableIds: List<UUID>
+//    ): List<Event>
 
 }
