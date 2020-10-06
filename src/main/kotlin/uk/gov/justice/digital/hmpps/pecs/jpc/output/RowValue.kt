cfe7e1aee0cfc54d6d9b25f8b6d8917432f65311
Jonny Cavell <jonny.cavell@gmail.com>
2020-10-06 16:48:50 +0100

    P4-2188 Write journeys to spreadsheet
    Write journeys to the spreadshet
    Write moves in a simpler way
    
    Add localstack jsonl files back (and remove jsonl from .gitignore)
    
    Remove jpc service from docker-compose.yml

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/output/RowValue.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/output/RowValue.kt
@@ -1,11 +1,12 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc.output
 
+import uk.gov.justice.digital.hmpps.pecs.jpc.calculator.JourneyPrice
 import uk.gov.justice.digital.hmpps.pecs.jpc.calculator.MovePrice
 import uk.gov.justice.digital.hmpps.pecs.jpc.reporting.Event
 import uk.gov.justice.digital.hmpps.pecs.jpc.reporting.EventType
 import java.time.format.DateTimeFormatter
 
-data class StandardValues(
+data class RowValue(
         val ref: String,
         val pickUp: String?,
         val pickUpLocationType: String?,
@@ -17,22 +18,22 @@ data class StandardValues(
         val dropOffTime: String?,
         val vehicleReg: String?,
         val prisonNumber: String?,
-        val totalInPence: Int?
+        val priceInPence: Int?
 ){
 
     companion object {
         val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
         val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
 
-        fun fromMovePrice(price: MovePrice): StandardValues {
+        fun forMovePrice(price: MovePrice): RowValue {
             val pickUpDate = Event.getLatestByType(price.moveReport.events, EventType.MOVE_START)?.occurredAt
             val dropOffDate = Event.getLatestByType(price.moveReport.events, EventType.MOVE_COMPLETE)?.occurredAt
 
             return with(price) {
-                StandardValues(
+                RowValue(
                         moveReport.move.reference,
-                        moveReport.move.fromLocation?.siteName,
-                        moveReport.move.fromLocation?.locationType?.name,
+                        moveReport.move.fromLocation.siteName,
+                        moveReport.move.fromLocation.locationType.name,
                         moveReport.move.toLocation?.siteName,
                         moveReport.move.toLocation?.locationType?.name,
                         pickUpDate?.format(dateFormatter),
@@ -43,6 +44,28 @@ data class StandardValues(
                             it.value.journey.vehicleRegistration ?: "NOT GIVEN"},
                         moveReport.person?.prisonNumber,
                         totalInPence()
+                )
+            }
+        }
+
+        fun forJourneyPrices(journeyNumber: Int, price: JourneyPrice): RowValue {
+            with(price) {
+                val pickUpDate = Event.getLatestByType(price.journeyWithEvents.events, EventType.MOVE_START)?.occurredAt
+                val dropOffDate = Event.getLatestByType(price.journeyWithEvents.events, EventType.MOVE_COMPLETE)?.occurredAt
+
+                return RowValue(
+                        "Journey $journeyNumber ",
+                        journeyWithEvents.journey.fromLocation.siteName,
+                        journeyWithEvents.journey.fromLocation.locationType.name,
+                        journeyWithEvents.journey.toLocation.siteName,
+                        journeyWithEvents.journey.toLocation.locationType.name,
+                        pickUpDate?.format(dateFormatter),
+                        pickUpDate?.format(timeFormatter),
+                        dropOffDate?.format(dateFormatter),
+                        dropOffDate?.format(timeFormatter),
+                        journeyWithEvents.journey.vehicleRegistration,
+                        null,
+                        priceInPence
                 )
             }
         }
