d7b8ce16131930b2cec5b5767b154ca9ef40adcd
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2020-10-07 15:56:36 +0100

    P4-2214 changes to include the prices used for the calculations to an extra tab in the output spreadsheet. (#49)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/config/GeoamyPricesProvider.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/config/GeoamyPricesProvider.kt
@@ -6,5 +6,8 @@ import java.io.InputStream
  * Responsible for providing the Geoamey prices Excel spreadsheet via an [InputStream].
  */
 fun interface GeoamyPricesProvider {
+    /**
+     * The caller is responsible for closing the [InputStream].
+     */
     fun get(): InputStream
 }
