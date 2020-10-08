79ce58dd721baac15e620dc6ec69f08168bad6b0
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-10-08 16:06:35 +0100

    P4-2214 small changes to apply stying to the journey id and price columns in the JPC price sheet.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/output/JPCPriceBookSheet.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/output/JPCPriceBookSheet.kt
@@ -1,34 +1,41 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc.output
 
-import org.apache.poi.ss.usermodel.Cell
-import org.apache.poi.ss.usermodel.CellType
-import org.apache.poi.ss.usermodel.Row
-import org.apache.poi.ss.usermodel.Sheet
-import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.ss.usermodel.*
 
 /**
  * Sheet to represent the actual JPC supplier prices used for all of the calculations in the outputted spreadsheet.
  */
 internal class JPCPriceBookSheet(workbook: Workbook) {
 
-    internal val sheet: Sheet = workbook.getSheet("JPC Price book")!!
+    private val journeyId: CellStyle = workbook.createCellStyle().apply {
+        this.dataFormat = workbook.createDataFormat().getFormat("\"JID_\"00000")
+    }
+
+    private val money: CellStyle = workbook.createCellStyle().apply {
+        this.dataFormat = workbook.createDataFormat().getFormat("£#,##0.00")
+    }
+
+    internal val sheet: Sheet = workbook.getSheet("JPC Price book")
 
     internal fun copyPricesFrom(originalPricesSheet: Sheet) {
         originalPricesSheet.copyTo(sheet)
     }
 
-    private infix fun Sheet.copyTo(destination: Sheet) {
+    private fun Sheet.copyTo(destination: Sheet) {
         this.forEach { sourceRow -> sourceRow.copyTo(destination.createRow(sourceRow.rowNum)) }
     }
 
-    private infix fun Row.copyTo(destination: Row) {
+    private fun Row.copyTo(destination: Row) {
         this.forEach { sourceCell -> sourceCell.copyTo(destination.createCell(sourceCell.columnIndex)) }
     }
 
-    private infix fun Cell.copyTo(destination: Cell) {
+    private fun Cell.copyTo(destination: Cell) {
         when (this.cellType) {
             CellType.STRING -> destination.setCellValue(this.stringCellValue)
-            CellType.NUMERIC -> destination.setCellValue(this.numericCellValue)
+            CellType.NUMERIC -> {
+                destination.setCellValue(this.numericCellValue)
+                if (this.columnIndex == 0) destination.cellStyle = journeyId else destination.cellStyle = money
+            }
             else -> return
         }
     }
