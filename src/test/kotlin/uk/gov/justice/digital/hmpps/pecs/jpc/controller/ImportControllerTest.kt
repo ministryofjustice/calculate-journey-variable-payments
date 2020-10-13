1100aa8bc28bceb8cfe170544e89552170d364a1
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-10-13 13:58:14 +0100

    P4-2235 verifying outputted file name in unit test.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/controller/ImportControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/controller/ImportControllerTest.kt
@@ -13,12 +13,15 @@ import org.springframework.test.context.ContextConfiguration
 import uk.gov.justice.digital.hmpps.pecs.jpc.TestConfig
 import uk.gov.justice.digital.hmpps.pecs.jpc.location.LocationRepository
 import uk.gov.justice.digital.hmpps.pecs.jpc.pricing.PriceRepository
+import java.time.Clock
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
 @ContextConfiguration(classes = [TestConfig::class])
-class ImportControllerTest(@Autowired val restTemplate: TestRestTemplate) {
+class ImportControllerTest(@Autowired val restTemplate: TestRestTemplate, @Autowired private val clock: Clock) {
 
     @Autowired
     lateinit var locationRepository: LocationRepository
@@ -30,8 +33,11 @@ class ImportControllerTest(@Autowired val restTemplate: TestRestTemplate) {
     fun `can generate spreadsheet for serco`() {
         assertThat(locationRepository.count()).isEqualTo(0)
         assertThat(priceRepository.count()).isEqualTo(0)
-        val response = restTemplate.getForEntity("/generate-prices-spreadsheet/SERCO?moves_from=2020-10-01&moves_to=2020-10-01&reports_to=2020-10-01", InputStreamResource::class.java)
 
+        val response = restTemplate.getForEntity("/generate-prices-spreadsheet/SERCO?moves_from=2020-10-01&moves_to=2020-10-01&reports_to=2020-10-01", InputStreamResource::class.java)
+        val uploadDateTime = LocalDateTime.now(clock).format(DateTimeFormatter.ofPattern("YYYY-MM-DD_HH_MM"))
+
+        assertThat(response.headers.contentDisposition.filename).isEqualTo("Journey_Variable_Payment_Output_SERCO_${uploadDateTime}.xlsx")
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(locationRepository.count()).isEqualTo(2)
         assertThat(priceRepository.count()).isEqualTo(1)
