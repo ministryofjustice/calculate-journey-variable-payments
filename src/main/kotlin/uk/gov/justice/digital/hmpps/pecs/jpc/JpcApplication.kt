2322c5c48dd70185175f23352a327a9e8e5fdcad
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2020-11-04 10:57:49 +0000

    P4-2434 adding ability to run an import on application start. (#76)
    * P4-2434 adding ability to run an import on application start.
    
    * P4-2434 renaming the import parameter to be specific to locations and prices.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/JpcApplication.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/JpcApplication.kt
@@ -1,11 +1,20 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc
 
+import org.springframework.beans.factory.getBean
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import uk.gov.justice.digital.hmpps.pecs.jpc.import.LocationAndPriceImporter
 
 @SpringBootApplication
 class JpcApplication
 
 fun main(args: Array<String>) {
-	runApplication<JpcApplication>(*args)
+    runApplication<JpcApplication>(*args).let { context ->
+
+        // This is a temporary solution to run an import of locations and prices then terminate bypassing the need to go to an endpoint.
+        args.firstOrNull { it == "--import-locations-and-prices" }?.let {
+            (context.getBean(LocationAndPriceImporter::class) as LocationAndPriceImporter).let { SpringApplication.exit(context, it) }
+        }
+    }
 }
