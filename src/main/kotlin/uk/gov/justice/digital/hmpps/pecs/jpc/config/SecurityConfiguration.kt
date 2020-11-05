cb00c2d6453625acd0be06fab235a0ccbd680864
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-11-05 16:32:08 +0000

    P4-2434 removal of import supplier reports endpoint as to be run via commandline.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/config/SecurityConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/config/SecurityConfiguration.kt
@@ -1,13 +1,13 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc.config
 
-import org.springframework.context.annotation.Profile
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.web.servlet.invoke
 
-// TODO for now this is tied to run for localstack profile only!!
 @EnableWebSecurity
+@ConditionalOnWebApplication
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
 
     @Throws(Exception::class)
