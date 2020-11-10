26329519e15cde76509cbfbd3683cfb09110a0f1
Jonny Cavell <jonny.cavell@gmail.com>
2020-11-10 12:05:03 +0000

    P4-2426 list moves
    Improve date session logic - put default in ChooseSupplierFilter.kt
    
    Use SessionAttributes so that we can use the model for session attributes transparently
    
    New addStartAndEndDatesToModel method looks to see if the request param has a value
    If it does, add this to the model / session
    
    Refactor to use constants

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/filter/ChooseSupplierFilter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/filter/ChooseSupplierFilter.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.pecs.jpc.filter
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import java.io.IOException
+import java.time.LocalDate
 import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -16,10 +17,11 @@ class ChooseSupplierFilter : Filter {
     override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
         val req = request as HttpServletRequest
         val res = response as HttpServletResponse
-        val session = req.getSession()
+        val session = req.session
         val supplier = session.getAttribute("supplier")
 
         if (supplier == null) {
+            session.setAttribute("date", LocalDate.now().withDayOfMonth(1))
             res.sendRedirect("/choose-supplier");
         } else {
             chain.doFilter(request, response)
