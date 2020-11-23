ddc3d0d266c6752d4780df4e3408fb0de499918f
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-11-23 15:34:25 +0000

    P4-2452 splitting out the imports further in effort to stop out of memory errors.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/JpcApplication.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/JpcApplication.kt
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import uk.gov.justice.digital.hmpps.pecs.jpc.importer.LocationAndPriceImporter
+import uk.gov.justice.digital.hmpps.pecs.jpc.importer.ManualLocationImporter
+import uk.gov.justice.digital.hmpps.pecs.jpc.importer.ManualPriceImporter
 import uk.gov.justice.digital.hmpps.pecs.jpc.importer.ReportsImporter
+import uk.gov.justice.digital.hmpps.pecs.jpc.price.Supplier
 import java.time.LocalDate
 
 @SpringBootApplication
@@ -14,8 +17,16 @@ class JpcApplication
 fun main(args: Array<String>) {
     runApplication<JpcApplication>(*args).let { context ->
         // This is a temporary solution to run an import of locations and prices then terminate bypassing the need to go to an endpoint.
-        context.environment.getProperty("import-locations-and-prices")?.let {
-            (context.getBean(LocationAndPriceImporter::class) as LocationAndPriceImporter).let { SpringApplication.exit(context, it.import()) }
+        context.environment.getProperty("import-locations")?.let {
+            (context.getBean(ManualLocationImporter::class) as ManualLocationImporter).let { SpringApplication.exit(context, it.import()) }
+        }
+
+        context.environment.getProperty("import-serco-prices")?.let {
+            (context.getBean(ManualPriceImporter::class) as ManualPriceImporter).let { SpringApplication.exit(context, it.import(Supplier.SERCO)) }
+        }
+
+        context.environment.getProperty("import-geoamey-prices")?.let {
+            (context.getBean(ManualPriceImporter::class) as ManualPriceImporter).let { SpringApplication.exit(context, it.import(Supplier.GEOAMEY)) }
         }
 
         // This is a temporary solution to run an import of both supplier reports then terminate bypassing the need to go to an endpoint.
