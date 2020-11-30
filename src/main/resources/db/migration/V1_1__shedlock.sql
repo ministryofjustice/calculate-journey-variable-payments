3694624fd47ab634c188865581f335fbe2a1878b
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2020-11-30 16:53:21 +0000

    P4-2535 correcting the sql script for the shedlock table. (#122)

--- src/main/resources/db/migration/V1_1__shedlock.sql
+++ src/main/resources/db/migration/V1_1__shedlock.sql
@@ -1,7 +1,6 @@
-CREATE TABLE SHEDLOCK
-(
-    name       VARCHAR(64) PRIMARY KEY,
-    lock_until TIMESTAMP(3) NULL,
-    locked_at  TIMESTAMP(3) NULL,
-    locked_by  VARCHAR(255) NOT NULL
+CREATE TABLE SHEDLOCK(
+    name VARCHAR(64) NOT NULL,
+    lock_until TIMESTAMP NOT NULL,
+    locked_at TIMESTAMP NOT NULL,
+    locked_by VARCHAR(255) NOT NULL, PRIMARY KEY (name)
 );
