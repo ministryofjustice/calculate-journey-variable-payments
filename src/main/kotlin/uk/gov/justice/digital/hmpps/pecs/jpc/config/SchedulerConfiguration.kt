9b998d6ee0b9df81d24b8ff89d7538435015d842
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-11-30 16:30:53 +0000

    P4-2535 correcting the sql script for the shedlock table.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/config/SchedulerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/config/SchedulerConfiguration.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.pecs.jpc.config
 import net.javacrumbs.shedlock.core.LockProvider
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -15,6 +16,13 @@ import javax.sql.DataSource
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtLeastFor = "PT1M", defaultLockAtMostFor = "PT30M")
 class SchedulerConfiguration {
+
+  private val logger = LoggerFactory.getLogger(javaClass)
+
   @Bean
-  fun lockProvider(dataSource: DataSource): LockProvider = JdbcTemplateLockProvider(dataSource)
+  fun lockProvider(dataSource: DataSource): LockProvider {
+    logger.info("Configuring scheduler lock provider.")
+
+    return JdbcTemplateLockProvider(dataSource)
+  }
 }
