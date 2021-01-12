4dc694be29f6baa014e3316ef8516d2ecab8c434
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2021-01-12 14:05:55 +0000

    P4-2546 work in progress for seeding some test data into dev env (note this is just for localstack at the moment). (#173)

---
+++ src/main/resources/db/migration/localstack/R__1_1__locations.sql
@@ -0,0 +1,5 @@
+insert into public.locations (location_id, added_at, location_type, nomis_agency_id, site_name)
+ values ('709fbee3-7fe6-4584-a8dc-f12481165bfa', current_timestamp, 'PR', 'GNI', 'HMP Grendon');
+
+insert into public.locations (location_id, added_at, location_type, nomis_agency_id, site_name)
+ values ('13c46837-c5c9-45a4-83d5-5a0d1438ff3c', current_timestamp, 'PR', 'WYI', 'HMYOI Wetherby');
---
+++ src/main/resources/db/migration/localstack/R__2_1__prices.sql
@@ -0,0 +1,2 @@
+insert into prices (price_id, added_at, effective_year, price_in_pence, supplier, from_location_id, to_location_id)
+ values ('9a67ab84-3f07-4d70-9c55-9e829552fcfb', current_timestamp, 2020, 4300, 'GEOAMEY', '13c46837-c5c9-45a4-83d5-5a0d1438ff3c', '709fbee3-7fe6-4584-a8dc-f12481165bfa');
