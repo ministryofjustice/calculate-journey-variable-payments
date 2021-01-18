d7024e43f209cb29a301d412f2a5dad823fa302f
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2021-01-18 14:25:33 +0000

    P4-2546 futher tweaks and alterations to the fake data for local dev and dev environment. (#180)

--- src/main/resources/db/migration/data/localstack/R__2_2__profiles.sql
+++ src/main/resources/db/migration/data/localstack/R__2_2__profiles.sql
@@ -1,3 +1,3 @@
-insert into profiles (profile_id, person_id, updated_at) values ('PR1', 'PE1', current_timestamp);
-insert into profiles (profile_id, person_id, updated_at) values ('PR2', 'PE2', current_timestamp);
-insert into profiles (profile_id, person_id, updated_at) values ('PR3', 'PE3', current_timestamp);
+insert into profiles (profile_id, person_id, updated_at) values ('PR1', 'PE1', '2020-06-16 10:20:30.000000');
+insert into profiles (profile_id, person_id, updated_at) values ('PR2', 'PE2', '2020-06-16 10:20:30.000000');
+insert into profiles (profile_id, person_id, updated_at) values ('PR3', 'PE3', '2020-06-16 10:20:30.000000');
