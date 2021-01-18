ee165aff845abaa0570417612101e15036679734
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2021-01-18 13:59:24 +0000

    P4-2546 futher tweaks and alterations to the fake data for local dev and dev environment.

--- src/main/resources/db/migration/data/localstack/R__1_2__people.sql
+++ src/main/resources/db/migration/data/localstack/R__1_2__people.sql
@@ -1,8 +1,3 @@
-insert into people (person_id, date_of_birth, ethnicity, first_names, gender, last_name, latest_nomis_booking_id, prison_number, updated_at)
- values ('PE1', '1980-12-25', 'White American', 'Billy the', 'male', 'Kid', null, 'PRISON1', '2020-06-16 10:20:30.000000');
-
-insert into people (person_id, date_of_birth, ethnicity, first_names, gender, last_name, latest_nomis_booking_id, prison_number, updated_at)
-values ('PE2', '1909-03-24', 'White American', 'Clyde Chestnut', 'male', 'Barrow', null, 'PRISON2', '2020-06-16 10:20:30.000000');
-
-insert into people (person_id, date_of_birth, ethnicity, first_names, gender, last_name, latest_nomis_booking_id, prison_number, updated_at)
- values ('PE3', '1910-10-01', 'White American', 'Bonnie Elizabeth', 'female', 'Parker', null, 'PRISON2', '2020-06-16 10:20:30.000000');
+insert into people (person_id, date_of_birth, ethnicity, first_names, gender, last_name, latest_nomis_booking_id, prison_number, updated_at) values ('PE1', '1980-12-25', 'White American', 'Billy the', 'male', 'Kid', null, 'PRISONER1', '2020-06-16 10:20:30.000000');
+insert into people (person_id, date_of_birth, ethnicity, first_names, gender, last_name, latest_nomis_booking_id, prison_number, updated_at) values ('PE2', '1909-03-24', 'White American', 'Clyde Chestnut', 'male', 'Barrow', null, 'PRISONER2', '2020-06-16 10:20:30.000000');
+insert into people (person_id, date_of_birth, ethnicity, first_names, gender, last_name, latest_nomis_booking_id, prison_number, updated_at) values ('PE3', '1910-10-01', 'White American', 'Bonnie Elizabeth', 'female', 'Parker', null, 'PRISONER3', '2020-06-16 10:20:30.000000');
