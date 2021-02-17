415394e7028b4cbcf4a1434752cde475c51eab69
Adam Sharp <adam.sharp@digital.justice.gov.uk>
2021-02-17 10:58:53 +0000

    Add moves in dev migrations, so that the price already exists error can be tested in dev

--- src/main/resources/db/migration/ddl/V1_2__alter_column_lengths.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE moves ALTER COLUMN cancellation_reason_comment TYPE varchar(1024);
-
-ALTER TABLE moves ALTER COLUMN notes TYPE varchar(1024);
-
-ALTER TABLE events ALTER COLUMN notes TYPE varchar(1024);
-
-ALTER TABLE journeys ALTER COLUMN notes TYPE varchar(1024);
-
