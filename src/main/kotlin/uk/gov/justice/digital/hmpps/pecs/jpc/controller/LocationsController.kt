104da757d715bc1de3a7f30d9ab06de0a97bd6c2
Adam Sharp <adam.sharp@digital.justice.gov.uk>
2021-03-23 12:30:59 +0000

    Resolve PR comments

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/controller/LocationsController.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.pecs.jpc.controller
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.pecs.jpc.service.MapFriendlyLocationService
+
+@RestController
+@RequestMapping(name = "Locations", path = ["/locations"], produces = [MediaType.APPLICATION_JSON_VALUE])
+class LocationsController(@Autowired val locationService: MapFriendlyLocationService) {
+  private val logger = LoggerFactory.getLogger(javaClass)
+
+  @GetMapping
+  fun all(@RequestParam(name = "version") userVersion: Long): LocationsDto {
+    logger.info("getting locations")
+
+    val locationsVersion = locationService.getVersion()
+
+    return LocationsDto(
+      locationsVersion,
+      if (userVersion != locationsVersion) locationService.findAll().associateBy({ it.nomisAgencyId }, { it.siteName })
+      else null
+    )
+  }
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  data class LocationsDto(val version: Long, val locations: Map<String, String>? = null)
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/controller/LocationsControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/controller/LocationsControllerTest.kt
@@ -23,7 +23,7 @@ import uk.gov.justice.digital.hmpps.pecs.jpc.service.MapFriendlyLocationService
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @ContextConfiguration(classes = [TestConfig::class])
-class LocationsJSONControllerTest(@Autowired private val wac: WebApplicationContext) {
+class LocationsControllerTest(@Autowired private val wac: WebApplicationContext) {
 
   private val mockMvc = MockMvcBuilders.webAppContextSetup(wac).build()
   private val mockSession = MockHttpSession(wac.servletContext)
@@ -37,25 +37,44 @@ class LocationsJSONControllerTest(@Autowired private val wac: WebApplicationCont
   }
 
   @Test
-  internal fun `locations_json with old version number returns locations and version number`() {
+  internal fun `get with old version number returns locations and version number`() {
     whenever(locationService.getVersion()).thenReturn(12345)
     whenever(locationService.findAll()).thenReturn(listOf(Location(LocationType.PR, "TESTLOC1", "Test Loc 1")))
 
-    mockMvc.get("/locations.json") {
+    mockMvc.get("/locations") {
       session = mockSession
-      param("locationsVersion", "-1")
+      param("version", "-1")
     }
       .andExpect { content { json("{version:12345,locations:{TESTLOC1:\"Test Loc 1\"}}") } }
       .andExpect { status { is2xxSuccessful() } }
   }
 
   @Test
-  internal fun `locations_json with newest version number returns just version number`() {
+  internal fun `get with old version number returns multiple locations and version number`() {
+    whenever(locationService.getVersion()).thenReturn(12345)
+    whenever(locationService.findAll()).thenReturn(
+      listOf(
+        Location(LocationType.PR, "TESTLOC1", "Test Loc 1"),
+        Location(LocationType.PR, "TESTLOC2", "Test Loc 2"),
+        Location(LocationType.PR, "TESTLOC3", "Test Loc 3")
+      )
+    )
+
+    mockMvc.get("/locations") {
+      session = mockSession
+      param("version", "-1")
+    }
+      .andExpect { content { json("{version:12345,locations:{TESTLOC1:\"Test Loc 1\",TESTLOC2:\"Test Loc 2\",TESTLOC3:\"Test Loc 3\"}}") } }
+      .andExpect { status { is2xxSuccessful() } }
+  }
+
+  @Test
+  internal fun `get with newest version number returns just version number`() {
     whenever(locationService.getVersion()).thenReturn(12345)
 
-    mockMvc.get("/locations.json") {
+    mockMvc.get("/locations") {
       session = mockSession
-      param("locationsVersion", "12345")
+      param("version", "12345")
     }
       .andExpect { content { json("{version:12345}") } }
       .andExpect { status { is2xxSuccessful() } }
