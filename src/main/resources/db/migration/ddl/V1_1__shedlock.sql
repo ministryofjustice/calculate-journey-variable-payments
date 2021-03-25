cd28236fb7d9ef028b9d43e42cf472ffdad9a871
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2021-03-25 14:01:29 +0000

    P4-2721 add flyway baseline to easily migrations going forwards. (#241)

--- src/main/resources/db/migration/ddl/V1_1__shedlock.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE SHEDLOCK(
-    name VARCHAR(64) NOT NULL,
-    lock_until TIMESTAMP NOT NULL,
-    locked_at TIMESTAMP NOT NULL,
-    locked_by VARCHAR(255) NOT NULL, PRIMARY KEY (name)
-);
---
+++ src/main/resources/db/migration/ddl/V1__baseline.sql
@@ -0,0 +1,128 @@
+create table audit_events
+(
+    audit_event_id uuid         not null,
+    created_at     timestamp    not null,
+    event_type     varchar(255) not null,
+    metadata       varchar(1024),
+    username       varchar(255) not null,
+    constraint AUDIT_EVENTS_PK primary key (audit_event_id)
+);
+
+create table events
+(
+    event_id       varchar(255) not null,
+    eventable_id   varchar(255),
+    eventable_type varchar(255),
+    notes          varchar(1024),
+    occurred_at    timestamp,
+    recorded_at    timestamp,
+    supplier       varchar(255),
+    event_type     varchar(255),
+    updated_at     timestamp,
+    constraint EVENTS_PK primary key (event_id)
+);
+
+create table journeys
+(
+    journey_id           varchar(255) not null,
+    billable             boolean      not null,
+    client_timestamp     timestamp,
+    drop_off             timestamp,
+    effective_year       int4         not null,
+    from_nomis_agency_id varchar(255) not null,
+    move_id              varchar(255) not null,
+    notes                varchar(1024),
+    pick_up              timestamp,
+    state                varchar(255) not null,
+    supplier             varchar(255) not null,
+    to_nomis_agency_id   varchar(255),
+    updated_at           timestamp,
+    vehicle_registration varchar(255),
+    constraint JOURNEYS_PK primary key (journey_id)
+);
+
+create table locations
+(
+    location_id     uuid         not null,
+    added_at        timestamp    not null,
+    location_type   varchar(255) not null,
+    nomis_agency_id varchar(255) not null,
+    site_name       varchar(255) not null,
+    updated_at      timestamp    not null,
+    constraint LOCATIONS_PK primary key (location_id),
+    constraint LOCATIONS_NAI_UNIQUE unique (nomis_agency_id),
+    constraint LOCATIONS_SN_UNIQUE unique (site_name)
+);
+
+create table moves
+(
+    move_id                     varchar(255)  not null,
+    cancellation_reason         varchar(255),
+    cancellation_reason_comment varchar(1024),
+    drop_off_or_cancelled       timestamp,
+    from_nomis_agency_id        varchar(255)  not null,
+    move_date                   date,
+    move_type                   varchar(255),
+    notes                       varchar(1024) not null,
+    pick_up                     timestamp,
+    profile_id                  varchar(255),
+    reference                   varchar(255)  not null,
+    report_from_location_type   varchar(255)  not null,
+    report_to_location_type     varchar(255),
+    status                      varchar(255)  not null,
+    supplier                    varchar(255)  not null,
+    to_nomis_agency_id          varchar(255),
+    updated_at                  timestamp     not null,
+    vehicle_registration        varchar(255),
+    constraint MOVES_PK primary key (move_id)
+);
+
+create table people
+(
+    person_id               varchar(255) not null,
+    date_of_birth           date,
+    ethnicity               varchar(255),
+    first_names             varchar(255),
+    gender                  varchar(255),
+    last_name               varchar(255),
+    latest_nomis_booking_id int4,
+    prison_number           varchar(255),
+    updated_at              timestamp    not null,
+    constraint PEOPLE_PK primary key (person_id)
+);
+
+create table prices
+(
+    price_id         uuid not null,
+    added_at         timestamp,
+    effective_year   int4 not null,
+    price_in_pence   int4 not null,
+    supplier         varchar(255),
+    from_location_id uuid,
+    to_location_id   uuid,
+    constraint PRICES_PK primary key (price_id),
+    constraint SUPPLIER_FROM_TO_YEAR_UNIQUE unique (supplier, from_location_id, to_location_id, effective_year),
+    constraint FROM_LOCATION_FK foreign key (from_location_id) references locations,
+    constraint TO_LOCATION_FK foreign key (to_location_id) references locations
+);
+
+create table profiles
+(
+    profile_id varchar(255) not null,
+    person_id  varchar(255) not null,
+    updated_at timestamp    not null,
+    constraint PROFILES_PK primary key (profile_id)
+);
+
+create table shedlock
+(
+    name       VARCHAR(64)  NOT NULL,
+    lock_until TIMESTAMP    NOT NULL,
+    locked_at  TIMESTAMP    NOT NULL,
+    locked_by  VARCHAR(255) NOT NULL,
+    constraint SHEDLOCK_PK PRIMARY KEY (name)
+);
+
+create index EVENTS_EVENTABLE_ID_IDX on events (eventable_id);
+
+create index JOURNEYS_MOVE_ID_IDX on journeys (move_id);
