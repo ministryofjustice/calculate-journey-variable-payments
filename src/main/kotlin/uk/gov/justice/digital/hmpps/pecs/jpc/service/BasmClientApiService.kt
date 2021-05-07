835e9d53abb7728c05dd5a2349123b2bd953ba05
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2021-05-07 12:45:41 +0100

    P4 2832 retrieve new locations added to basm (#260)
    * P4-2832 part 1 to retrieve newly added locations in BaSM so we can automatically add them to JPC without the user having to do via UI.
    
    * P4-2832 part 2 to retrieve newly added locations.  Changes included mapping of court types and unsupport types.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/service/BasmClientApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/service/BasmClientApiService.kt
@@ -1,11 +1,19 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc.service
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.pecs.jpc.location.LocationType
 import java.time.Duration
+import java.time.LocalDate
 
 @Service
 class BasmClientApiService(
@@ -20,7 +28,7 @@ class BasmClientApiService(
     logger.info("Looking up location name for agency ID '${agencyId.trim().toUpperCase()}'.")
 
     fun recordIfLocationNotFound(location: LocationResponse?) {
-      if (location?.data.isNullOrEmpty()) {
+      if (location?.locations.isNullOrEmpty()) {
         logger.info("Location name for agency ID '${agencyId.trim().toUpperCase()}' not found on calling BaSM API.")
         monitoringService.capture("Location name for agency ID '${agencyId.trim().toUpperCase()}' not found on calling BaSM API.")
       }
@@ -40,13 +48,78 @@ class BasmClientApiService(
         .block(basmApiTimeout)
     }
       .onFailure { recordLocationLookupFailure(it) }
-      .onSuccess { recordIfLocationNotFound(it) }.getOrNull()?.data?.elementAtOrNull(0)?.name()?.toUpperCase()
+      .onSuccess { recordIfLocationNotFound(it) }.getOrNull()?.locations?.elementAtOrNull(0)?.name?.toUpperCase()
+  }
+
+  fun findNomisAgenciesCreatedOn(date: LocalDate): List<BasmNomisLocation> {
+    fun recordFailure(error: Throwable) {
+      logger.error("An error occurred trying to find location by date '$date' on calling BaSM API: ${error.message}")
+      monitoringService.capture("An error occurred trying to find location by date '$date' on calling BaSM API: ${error.message}")
+    }
+
+    return Result.runCatching {
+      basmApiWebClient
+        .get()
+        .uri("api/reference/locations?filter[created_at]=$date")
+        .retrieve()
+        .bodyToMono(LocationResponse::class.java)
+        .map { it.locations }
+        .block(basmApiTimeout)
+    }
+      .onFailure { recordFailure(it) }
+      .getOrDefault(listOf()).filterNotNull()
   }
 }
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-private data class LocationResponse(val data: List<Location> = listOf())
+data class LocationResponse(
+  @JsonProperty("data")
+  @JsonDeserialize(contentUsing = NomisLocationDeserializer::class)
+  val locations: List<BasmNomisLocation> = listOf()
+)
 
-private data class Location(val attributes: Map<Any, Any>) {
-  fun name(): String = attributes.getOrDefault("title", "unknown") as String
+data class BasmNomisLocation(
+  val name: String,
+  val agencyId: String,
+  val locationType: LocationType,
+  val createdAt: LocalDate
+)
+
+object NomisLocationDeserializer : JsonDeserializer<BasmNomisLocation>() {
+
+  private val mayBe: Map<String, LocationType> = mapOf(
+    "approved_premises" to LocationType.APP,
+    "hospital" to LocationType.HP,
+    "immigration_detention_centre" to LocationType.IM,
+    "high_security_hospital" to LocationType.HP,
+    "police" to LocationType.PS,
+    "prison" to LocationType.PR,
+    "probation_office" to LocationType.PB,
+    "secure_childrens_home" to LocationType.SCH,
+    "secure_training_centre" to LocationType.STC
+  )
+
+  override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): BasmNomisLocation? =
+    (p?.readValueAsTree() as JsonNode)["attributes"]?.let { attributes ->
+      val title = attributes["title"].asText().trim().toUpperCase()
+      val agencyId = attributes["nomis_agency_id"].asText().trim().toUpperCase()
+      val locationType = attributes["location_type"].asText()
+      val createdAt = LocalDate.parse(attributes["created_at"].asText())
+
+      return (mayBe[locationType] ?: mayBeCourt(locationType, title))?.let { BasmNomisLocation(title, agencyId, it, createdAt) }
+    }
+
+  private fun mayBeCourt(type: String, title: String): LocationType? {
+    return if (type == "court") {
+      val sanitisedTitle = title.toUpperCase()
+
+      when {
+        sanitisedTitle.contains("COMBINED COURT") -> LocationType.CM
+        sanitisedTitle.contains("COUNTY COURT") -> LocationType.CO
+        sanitisedTitle.contains("CROWN COURT") -> LocationType.CC
+        sanitisedTitle.contains("MAGISTRATES COURT") -> LocationType.MC
+        else -> LocationType.CRT
+      }
+    } else null
+  }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/service/BasmClientApiServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/service/BasmClientApiServiceTest.kt
@@ -8,8 +8,13 @@ import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.pecs.jpc.location.LocationType
 import java.time.Duration
+import java.time.LocalDate
 import java.util.concurrent.TimeUnit
 
 internal class BasmClientApiServiceTest {
@@ -71,7 +76,7 @@ internal class BasmClientApiServiceTest {
   }
 
   @Test
-  internal fun `monitoring service is called when location when name not found`() {
+  internal fun `monitoring service is called when location name not found`() {
     basmApiServer.enqueue(locationResponse(body = "{ \"data\": [] }"))
 
     assertThat(service.findNomisAgencyLocationNameBy("NO MATCH")).isNull()
@@ -79,14 +84,122 @@ internal class BasmClientApiServiceTest {
   }
 
   @Test
-  internal fun `monitoring service is called on with call timeout`() {
+  internal fun `monitoring service is called with location lookup call timeout`() {
     basmApiServer.enqueue(locationResponse("timed out").setBodyDelay(1, TimeUnit.SECONDS))
 
     assertThat(service.findNomisAgencyLocationNameBy("TIMEOUT")).isNull()
     verify(monitoringService).capture(any())
   }
 
-  private fun locationResponse(locationName: String? = null, body: String = "{ \"data\": [ { \"attributes\": { \"title\": \"$locationName\" } } ] }") =
+  @ParameterizedTest
+  @MethodSource("locationMappingTestData")
+  fun `locations types are mapped to the correct type`(input: FakeNomisLocation, expected: BasmNomisLocation?) {
+    basmApiServer.enqueue(locationResponse(input.title, input.nomisAgencyId, input.locationType, input.createdAt))
+
+    val mappedLocation = service.findNomisAgenciesCreatedOn(input.createdAt)
+
+    assertThat(mappedLocation).containsExactly(expected)
+    verifyZeroInteractions(monitoringService)
+  }
+
+  @Test
+  fun `unrecognised locations are not mapped`() {
+    basmApiServer.enqueue(locationResponse(" unknown ", " court_agency_ID ", "unknown_type", LocalDate.of(2021, 5, 14)))
+
+    assertThat(service.findNomisAgenciesCreatedOn(LocalDate.of(2021, 5, 14))).isEmpty()
+    verifyZeroInteractions(monitoringService)
+  }
+
+  @Test
+  internal fun `monitoring service is called with getting created location call timeout`() {
+    basmApiServer.enqueue(locationResponse("timed out").setBodyDelay(1, TimeUnit.SECONDS))
+
+    assertThat(service.findNomisAgenciesCreatedOn(LocalDate.now())).isEmpty()
+    verify(monitoringService).capture(any())
+  }
+
+  private companion object {
+    @JvmStatic
+    fun locationMappingTestData(): List<Arguments> = listOf(
+      Arguments.of(
+        FakeNomisLocation(" Approved ", " Approved_agency_ID ", "approved_premises", LocalDate.of(2021, 5, 1)),
+        BasmNomisLocation("APPROVED", "APPROVED_AGENCY_ID", LocationType.APP, LocalDate.of(2021, 5, 1))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" Hospital ", " hospital_agency_ID ", "hospital", LocalDate.of(2021, 5, 2)),
+        BasmNomisLocation("HOSPITAL", "HOSPITAL_AGENCY_ID", LocationType.HP, LocalDate.of(2021, 5, 2))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" Police ", " police_agency_ID ", "police", LocalDate.of(2021, 5, 3)),
+        BasmNomisLocation("POLICE", "POLICE_AGENCY_ID", LocationType.PS, LocalDate.of(2021, 5, 3))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" Prison ", " prison_agency_ID ", "prison", LocalDate.of(2021, 5, 4)),
+        BasmNomisLocation("PRISON", "PRISON_AGENCY_ID", LocationType.PR, LocalDate.of(2021, 5, 4))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" Probation ", " probation_agency_ID ", "probation_office", LocalDate.of(2021, 5, 5)),
+        BasmNomisLocation("PROBATION", "PROBATION_AGENCY_ID", LocationType.PB, LocalDate.of(2021, 5, 5))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" Immigration ", " immigration_agency_ID ", "immigration_detention_centre", LocalDate.of(2021, 5, 6)),
+        BasmNomisLocation("IMMIGRATION", "IMMIGRATION_AGENCY_ID", LocationType.IM, LocalDate.of(2021, 5, 6))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" High Security Hospital ", " high_security_hospital_agency_ID ", "high_security_hospital", LocalDate.of(2021, 5, 7)),
+        BasmNomisLocation("HIGH SECURITY HOSPITAL", "HIGH_SECURITY_HOSPITAL_AGENCY_ID", LocationType.HP, LocalDate.of(2021, 5, 7))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" sch ", " sch_agency_ID ", "secure_childrens_home", LocalDate.of(2021, 5, 8)),
+        BasmNomisLocation("SCH", "SCH_AGENCY_ID", LocationType.SCH, LocalDate.of(2021, 5, 8))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" stc ", " stc_agency_ID ", "secure_training_centre", LocalDate.of(2021, 5, 9)),
+        BasmNomisLocation("STC", "STC_AGENCY_ID", LocationType.STC, LocalDate.of(2021, 5, 9))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" County courT ", " court_agency_ID ", "court", LocalDate.of(2021, 5, 10)),
+        BasmNomisLocation("COUNTY COURT", "COURT_AGENCY_ID", LocationType.CO, LocalDate.of(2021, 5, 10))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" combineD courT ", " court_agency_ID ", "court", LocalDate.of(2021, 5, 11)),
+        BasmNomisLocation("COMBINED COURT", "COURT_AGENCY_ID", LocationType.CM, LocalDate.of(2021, 5, 11))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" cRown courT ", " court_agency_ID ", "court", LocalDate.of(2021, 5, 12)),
+        BasmNomisLocation("CROWN COURT", "COURT_AGENCY_ID", LocationType.CC, LocalDate.of(2021, 5, 12))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" magistrates courT ", " court_agency_ID ", "court", LocalDate.of(2021, 5, 13)),
+        BasmNomisLocation("MAGISTRATES COURT", "COURT_AGENCY_ID", LocationType.MC, LocalDate.of(2021, 5, 13))
+      ),
+      Arguments.of(
+        FakeNomisLocation(" ranDom courT ", " court_agency_ID ", "court", LocalDate.of(2021, 5, 14)),
+        BasmNomisLocation("RANDOM COURT", "COURT_AGENCY_ID", LocationType.CRT, LocalDate.of(2021, 5, 14))
+      )
+    )
+  }
+
+  data class FakeNomisLocation(val title: String, val nomisAgencyId: String, val locationType: String, val createdAt: LocalDate)
+
+  private fun locationResponse(
+    title: String = "",
+    nomisAgencyId: String = "",
+    locationType: String = "police",
+    createdAt: LocalDate = LocalDate.now(),
+    body: String = """
+      { "data": [ 
+          { 
+            "attributes": {
+               "title": "$title",
+               "nomis_agency_id": "$nomisAgencyId",
+               "location_type": "$locationType",
+               "created_at": "$createdAt"
+            }
+          } 
+        ]
+      }"""
+  ) =
     MockResponse()
       .addHeader("Content-Type", "application/json; charset=utf-8")
       .setBody(body)
