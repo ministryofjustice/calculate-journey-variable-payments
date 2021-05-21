e4c60665cc7b5b70f5f840f262f849cf31d55b45
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2021-05-21 13:13:01 +0100

    P4-2895 migration to backfill location history audit events.  The audting functionality came after some of the locations were there.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/integration/MigrationsTest.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.pecs.jpc.integration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.context.TestPropertySource
+import uk.gov.justice.digital.hmpps.pecs.jpc.TestConfig
+import uk.gov.justice.digital.hmpps.pecs.jpc.util.PostgresContainer
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = [TestConfig::class])
+@TestPropertySource(properties = ["spring.flyway.enabled=true", "spring.flyway.baseline-on-migrate=true", ])
+internal class MigrationsTest {
+
+  @Test
+  internal fun `migrations run without any errors when running against a Postgres DB instance`() {
+    // no assertion necessary test will fail if any of the migrations fail to run e.g. malformed SQL
+  }
+
+  companion object {
+
+    private val pgContainer = PostgresContainer.instance
+
+    @JvmStatic
+    @DynamicPropertySource
+    fun properties(registry: DynamicPropertyRegistry) {
+      pgContainer?.run {
+        registry.add("spring.datasource.url", pgContainer::getJdbcUrl)
+        registry.add("spring.datasource.username", pgContainer::getUsername)
+        registry.add("spring.datasource.password", pgContainer::getPassword)
+        registry.add("spring.datasource.placeholders.database_update_password", pgContainer::getPassword)
+        registry.add("spring.datasource.placeholders.database_read_only_password", pgContainer::getPassword)
+        registry.add("spring.flyway.url", pgContainer::getJdbcUrl)
+        registry.add("spring.flyway.user", pgContainer::getUsername)
+        registry.add("spring.flyway.password", pgContainer::getPassword)
+      }
+    }
+  }
+}
--- src/test/resources/db/migration/testdata/V1_1_1__price_audit_event.sql
+++
@@ -1,8 +0,0 @@
-insert into audit_events values
-(
-    '28aca3e6-acc5-11eb-8529-0242ac130003',
-    '2021-04-22 14:01:58.468570',
-    'JOURNEY_PRICE',
-    '{"supplier": "SERCO", "fromNomisId": "PRISON1", "toNomisId": "PRISON2", "effectiveYear": 2020, "price": 10.50}',
-    'DUMMY USER'
-);
