ae7ea093754e9280f3e2266683a5410f3e661598
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2021-05-24 13:56:04 +0100

    P4-2895 tweaking migration to filter on the event type, if there was a price change then this was causing it to exclude add a default entry.

--- src/main/resources/db/migration/dml/V1_3__backfill_location_audit_events.sql
+++ src/main/resources/db/migration/dml/V1_3__backfill_location_audit_events.sql
@@ -7,6 +7,4 @@ insert into audit_events
          '{"nomis_id" : "'|| l.nomis_agency_id ||'", "new_name" : "'|| l.site_name ||'", "new_type" : "'|| l.location_type ||'"}',
          '_TERMINAL_'
     from locations l
-   where not exists (select 1 from audit_events ae where ae.metadata like '%"'|| l.nomis_agency_id ||'"%');
-
-select uuid_generate_v4(), l.added_at, 'LOCATION', '{"nomis_id" : "'|| l.nomis_agency_id ||'", "new_name" : "'|| l.site_name ||'", "new_type" : "'|| l.location_type ||'"}', '_TERMINAL_' from locations l where not exists (select 1 from audit_events ae where ae.metadata like '%"'|| l.nomis_agency_id ||'"%');
+   where not exists (select 1 from audit_events ae where ae.metadata like '%"'|| l.nomis_agency_id ||'"%' and ae.event_type = 'LOCATION');
