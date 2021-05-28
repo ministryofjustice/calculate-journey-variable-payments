baec745003a464aa1d52014a766b45bd66e340c6
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2021-05-28 09:02:51 +0100

    P4-2844 adding new location type coroners court.  This will automatcally be picked up by the UI template. (#278)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/location/LocationType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/location/LocationType.kt
@@ -10,6 +10,7 @@ enum class LocationType(val label: String) {
   CC("Crown Court"),
   CM("Combined Court"),
   CO("County Court"),
+  CRN("Coroners Court"),
   CRT("Court"),
   HP("Hospital"),
   IM("Immigration"),
