5df6e54888bd9748eda47484c05133be3b20141a
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2021-06-07 14:04:01 +0100

    P4-2848 changes to audit spreadsheet download failures. Monitoring is not required here, this is already handled via the higher level exception handler.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/service/SpreadsheetServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/service/SpreadsheetServiceTest.kt
@@ -2,6 +2,8 @@ package uk.gov.justice.digital.hmpps.pecs.jpc.service
 
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.springframework.security.core.Authentication
 import uk.gov.justice.digital.hmpps.pecs.jpc.auditing.AuditableEvent
@@ -37,6 +39,40 @@ internal class SpreadsheetServiceTest {
       AuditableEvent.downloadSpreadsheetEvent(
         LocalDate.of(2021, 2, 22),
         supplier,
+        authentication
+      )
+    )
+  }
+
+  @Test
+  internal fun `failure to download Serco spreadsheet is audited`() {
+    whenever(pricesSpreadsheetGenerator.generate(Supplier.SERCO, LocalDate.of(2021, 6, 7))).thenThrow(RuntimeException("spreadsheet download failed for Serco"))
+
+    assertThatThrownBy { service.spreadsheet(authentication, Supplier.SERCO, LocalDate.of(2021, 6, 7)) }
+      .isInstanceOf(RuntimeException::class.java)
+      .hasMessage("spreadsheet download failed for Serco")
+
+    verify(auditService).create(
+      AuditableEvent.downloadSpreadsheetFailure(
+        LocalDate.of(2021, 6, 7),
+        Supplier.SERCO,
+        authentication
+      )
+    )
+  }
+
+  @Test
+  internal fun `failure to download GEOAmey spreadsheet is audited`() {
+    whenever(pricesSpreadsheetGenerator.generate(Supplier.GEOAMEY, LocalDate.of(2021, 6, 6))).thenThrow(RuntimeException("spreadsheet download failed for GEOAmey"))
+
+    assertThatThrownBy { service.spreadsheet(authentication, Supplier.GEOAMEY, LocalDate.of(2021, 6, 6)) }
+      .isInstanceOf(RuntimeException::class.java)
+      .hasMessage("spreadsheet download failed for GEOAmey")
+
+    verify(auditService).create(
+      AuditableEvent.downloadSpreadsheetFailure(
+        LocalDate.of(2021, 6, 6),
+        Supplier.GEOAMEY,
         authentication
       )
     )
