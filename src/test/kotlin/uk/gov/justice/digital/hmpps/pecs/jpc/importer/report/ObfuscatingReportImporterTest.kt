a8adbb287efae7a189fa0d261c1f4cb873e58b28
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2021-06-08 14:26:32 +0100

    P4-2875 initial commit for PII obfuscation when running locally. (#280)

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/importer/report/ObfuscatingReportImporterTest.kt
@@ -0,0 +1,50 @@
+package uk.gov.justice.digital.hmpps.pecs.jpc.importer.report
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import uk.gov.justice.digital.hmpps.pecs.jpc.TestConfig
+import uk.gov.justice.digital.hmpps.pecs.jpc.config.ReportingProvider
+import uk.gov.justice.digital.hmpps.pecs.jpc.service.MonitoringService
+import java.time.LocalDate
+
+@ExtendWith(SpringExtension::class)
+@Import(TestConfig::class)
+@ActiveProfiles("test")
+internal class ObfuscatingReportImporterTest() {
+
+  @MockBean
+  private lateinit var monitoringService: MonitoringService
+
+  @Test
+  fun `personal identifiable information is obfuscated`() {
+    val prisonNumber = "PRISON1"
+    val firstName = "Fred"
+    val lastName = "Blogs"
+    val ethnicity = "European"
+
+    val obfuscatedPerson = ObfuscatingReportImporter(
+      personProvider(
+        prisonNumber = prisonNumber,
+        firstName = firstName,
+        lastName = lastName,
+        ethnicity = ethnicity
+      ),
+      monitoringService
+    ).importPeopleOn(LocalDate.now()).toList().first()
+
+    assertThat(obfuscatedPerson.dateOfBirth).isEqualTo(LocalDate.of(1800, 1, 1))
+    assertThat(obfuscatedPerson.prisonNumber?.trim()?.toUpperCase()).isNotEqualTo(prisonNumber.trim().toUpperCase())
+    assertThat(obfuscatedPerson.firstNames?.trim()?.toUpperCase()).isNotEqualTo(firstName.trim().toUpperCase())
+    assertThat(obfuscatedPerson.lastName?.trim()?.toUpperCase()).isNotEqualTo(lastName.trim().toUpperCase())
+    assertThat(obfuscatedPerson.ethnicity).isEqualTo("Other")
+  }
+
+  private fun personProvider(prisonNumber: String, firstName: String, lastName: String, ethnicity: String) = ReportingProvider {
+    """{"id":"PE1","updated_at": "2020-06-16T10:20:30+01:00", "prison_number":"$prisonNumber","latest_nomis_booking_id":null,"gender":"male","age":100, "ethnicity" : "White American", "first_names" : "$firstName", "last_name": "$lastName", "date_of_birth" : "1980-12-25"}""".trimIndent()
+  }
+}
