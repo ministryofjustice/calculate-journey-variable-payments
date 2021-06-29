0f06ab6c9881e007b5083f72e52da65768476fdc
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2021-06-29 11:42:31 +0100

    P4-2956 house keeping PR to update 3rd party dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.0"
-  kotlin("plugin.spring") version "1.5.10"
-  kotlin("plugin.jpa") version "1.5.10"
-  kotlin("plugin.allopen") version "1.5.10"
+  kotlin("plugin.spring") version "1.5.20"
+  kotlin("plugin.jpa") version "1.5.20"
+  kotlin("plugin.allopen") version "1.5.20"
 }
 
 allOpen {
@@ -19,7 +19,7 @@ dependencies {
   implementation("com.github.kittinunf.result:result:4.0.0")
   implementation("com.github.kittinunf.result:result-coroutines:4.0.0")
   implementation("com.beust:klaxon:5.5")
-  implementation("com.amazonaws:aws-java-sdk-s3:1.12.3")
+  implementation("com.amazonaws:aws-java-sdk-s3:1.12.14")
   implementation("io.sentry:sentry-spring-boot-starter:5.0.1")
   implementation("net.javacrumbs.shedlock:shedlock-spring:4.24.0")
   implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.24.0")
@@ -40,19 +40,16 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.session:spring-session-jdbc:2.5.0")
+  implementation("org.springframework.session:spring-session-jdbc:2.5.1")
   implementation("org.springframework.shell:spring-shell-starter:2.0.1.RELEASE")
   implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5:3.0.4.RELEASE")
   implementation(kotlin("script-runtime"))
 
   testImplementation("org.seleniumhq.selenium:selenium-support:3.141.59")
-  testImplementation("org.seleniumhq.selenium:selenium-chrome-driver:3.141.59")
-  testImplementation("org.seleniumhq.selenium:selenium-firefox-driver:3.141.59")
-
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.fluentlenium:fluentlenium-junit-jupiter:4.8.0")
   testImplementation("org.fluentlenium:fluentlenium-assertj:4.8.0")
-  testImplementation("org.mockito:mockito-inline:3.11.0")
+  testImplementation("org.mockito:mockito-inline:3.11.2")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.squareup.okhttp3:mockwebserver:4.9.1")
@@ -61,7 +58,7 @@ dependencies {
 
   runtimeOnly("org.flywaydb:flyway-core:7.10.0")
   runtimeOnly("com.h2database:h2")
-  runtimeOnly("org.postgresql:postgresql:42.2.20")
+  runtimeOnly("org.postgresql:postgresql:42.2.22")
 }
 
 tasks {
