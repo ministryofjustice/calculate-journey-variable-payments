b2042443a7d7368dd2ae6b61585ec838bb755a38
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2021-07-30 09:19:04 +0100

    P4-3011 changes to prevent price uplifts prior to the current effective.  We should never be doing uplifts prior to the current one.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/commands/BulkPriceUpdateCommands.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/commands/BulkPriceUpdateCommands.kt
@@ -1,29 +1,14 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc.commands
 
-import org.slf4j.LoggerFactory
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 import uk.gov.justice.digital.hmpps.pecs.jpc.price.Supplier
 import uk.gov.justice.digital.hmpps.pecs.jpc.service.AnnualPriceAdjustmentsService
-import uk.gov.justice.digital.hmpps.pecs.jpc.service.BulkPricesService
 
 @ShellComponent
 class BulkPriceUpdateCommands(
-  private val bulkPricesService: BulkPricesService,
   private val annualPriceAdjustmentsService: AnnualPriceAdjustmentsService
 ) {
-
-  private val logger = LoggerFactory.getLogger(javaClass)
-
-  @ShellMethod("Adds prices for the next effective year based on the current effective years prices and the supplied multiplier.")
-  fun addNextYearsPrices(supplier: Supplier, multiplier: Double) {
-    logger.info("Starting bulk price update for $supplier with multiplier $multiplier")
-
-    bulkPricesService.addNextYearsPrices(supplier, multiplier)
-
-    logger.info("Finished bulk price update for $supplier with multiplier $multiplier")
-  }
-
   @ShellMethod("Performs a price uplift for the given supplier, effective year and the supplied multiplier.")
   fun uplift(supplier: Supplier, effectiveYear: Int, multiplier: Double, force: Boolean = false) {
     if (force)
--- src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/service/AnnualPriceAdjustmentsServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/service/AnnualPriceAdjustmentsServiceTest.kt
@@ -6,21 +6,24 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.pecs.jpc.auditing.AuditEventType
 import uk.gov.justice.digital.hmpps.pecs.jpc.auditing.AuditableEvent
 import uk.gov.justice.digital.hmpps.pecs.jpc.config.TimeSource
 import uk.gov.justice.digital.hmpps.pecs.jpc.price.AnnualPriceAdjuster
+import uk.gov.justice.digital.hmpps.pecs.jpc.price.EffectiveYear
 import uk.gov.justice.digital.hmpps.pecs.jpc.price.PriceAdjustmentRepository
 import uk.gov.justice.digital.hmpps.pecs.jpc.price.PriceRepository
 import uk.gov.justice.digital.hmpps.pecs.jpc.price.Supplier
-import java.time.LocalDateTime
+import java.time.LocalDate
 
 internal class AnnualPriceAdjustmentsServiceTest {
 
   private val priceAdjustmentRepository: PriceAdjustmentRepository = mock()
   private val priceRepository: PriceRepository = mock()
-  private val timeSource = TimeSource { LocalDateTime.now() }
+  private val timeSource = TimeSource { LocalDate.of(2020, 1, 1).atStartOfDay() }
+  private val effectiveYear = EffectiveYear(timeSource)
   private val monitoringService: MonitoringService = mock()
   private val auditService: AuditService = mock()
   private val annualPriceAdjuster: AnnualPriceAdjuster = AnnualPriceAdjuster(priceRepository, priceAdjustmentRepository, auditService, timeSource)
@@ -28,7 +31,7 @@ internal class AnnualPriceAdjustmentsServiceTest {
 
   @Test
   internal fun `price uplift for Serco`() {
-    AnnualPriceAdjustmentsService(annualPriceAdjusterSpy, monitoringService, auditService).uplift(
+    AnnualPriceAdjustmentsService(annualPriceAdjusterSpy, monitoringService, auditService, effectiveYear).uplift(
       Supplier.SERCO,
       2020,
       1.0
@@ -39,7 +42,7 @@ internal class AnnualPriceAdjustmentsServiceTest {
 
   @Test
   internal fun `price uplift for GEOAmey`() {
-    AnnualPriceAdjustmentsService(annualPriceAdjusterSpy, monitoringService, auditService).uplift(
+    AnnualPriceAdjustmentsService(annualPriceAdjusterSpy, monitoringService, auditService, effectiveYear).uplift(
       Supplier.GEOAMEY,
       2021,
       2.0
@@ -52,7 +55,7 @@ internal class AnnualPriceAdjustmentsServiceTest {
   internal fun `monitoring service captures failed price uplift`() {
     whenever(priceAdjustmentRepository.saveAndFlush(any())).thenThrow(RuntimeException("something went wrong"))
 
-    AnnualPriceAdjustmentsService(annualPriceAdjuster, monitoringService, auditService).uplift(
+    AnnualPriceAdjustmentsService(annualPriceAdjuster, monitoringService, auditService, effectiveYear).uplift(
       Supplier.GEOAMEY,
       2021,
       2.0
@@ -63,7 +66,7 @@ internal class AnnualPriceAdjustmentsServiceTest {
 
   @Test
   internal fun `auditing service captures successful price uplift`() {
-    AnnualPriceAdjustmentsService(annualPriceAdjuster, monitoringService, auditService).uplift(
+    AnnualPriceAdjustmentsService(annualPriceAdjuster, monitoringService, auditService, effectiveYear).uplift(
       Supplier.GEOAMEY,
       2021,
       2.0
@@ -76,5 +79,16 @@ internal class AnnualPriceAdjustmentsServiceTest {
         mapOf("supplier" to Supplier.GEOAMEY, "effective_year" to 2021, "multiplier" to 2.0)
       )
     )
+  }
+
+  @Test
+  internal fun `cannot uplift years prior to the current effective year`() {
+    assertThatThrownBy {
+      AnnualPriceAdjustmentsService(annualPriceAdjuster, monitoringService, auditService, effectiveYear).uplift(
+        Supplier.GEOAMEY,
+        effectiveYear.current() - 1,
+        2.0
+      )
+    }.isInstanceOf(RuntimeException::class.java).hasMessage("Price uplifts cannot be before the current effective year ${effectiveYear.current()}.")
   }
 }
