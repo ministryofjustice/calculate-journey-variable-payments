56288d07ed195d9cebf8711a4f0a1bea9d6b5fc4
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2021-08-31 14:04:57 +0100

    P4-3009 small refactoring unreleated to adjustment audit history.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/controller/PriceHistoryDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/controller/PriceHistoryDto.kt
@@ -7,7 +7,11 @@ import uk.gov.justice.digital.hmpps.pecs.jpc.domain.price.Money
 import uk.gov.justice.digital.hmpps.pecs.jpc.domain.price.Supplier
 import java.time.LocalDateTime
 
-data class PriceHistoryDto(val datetime: LocalDateTime, val action: String, val by: String) {
+data class PriceHistoryDto(
+  val datetime: LocalDateTime,
+  val action: String,
+  val by: String
+) {
   companion object {
     /**
      * Throws a runtime exception if the [AuditEvent] is not a journey price event or if there is a supplier mismatch.
@@ -19,11 +23,12 @@ data class PriceHistoryDto(val datetime: LocalDateTime, val action: String, val 
       if (data.supplier != supplier) throw RuntimeException("Audit priced event not for supplier $supplier")
 
       val action = buildString {
-        if (data.isUpdate()) {
-          append("Price changed from £${Money.valueOf(data.oldPrice!!)} to £${Money.valueOf(data.newPrice)}. Effective from ${data.effectiveYear} to ${data.effectiveYear + 1}.")
-        } else {
-          append("Journey priced at £${Money.valueOf(data.newPrice)}. Effective from ${data.effectiveYear} to ${data.effectiveYear + 1}.")
-        }
+        append(
+          when {
+            data.isUpdate() -> "Price changed from £${Money.valueOf(data.oldPrice!!)} to £${Money.valueOf(data.newPrice)}. Effective from ${data.effectiveYear} to ${data.effectiveYear + 1}."
+            else -> "Journey priced at £${Money.valueOf(data.newPrice)}. Effective from ${data.effectiveYear} to ${data.effectiveYear + 1}."
+          }
+        )
       }
 
       return PriceHistoryDto(
