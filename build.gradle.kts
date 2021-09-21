3c3ffb89a240a1089696cb1c262e0f51faa15e09
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2021-09-21 08:15:33 +0100

    P4 3149 housekeeping (#347)
    * P4-3149 bumping to latest version of the HMPPS Gradle Spring Boot plugin.
    
    * P4-3149 resolves compiler warning - task (current target is 16) and 'compileKotlin' task (current target is 11)
    
    * P4-3149 bumpling to latest version of the Kotlin plugin.
    
    * P4-3149 removing unused 3rd party library.
    
    * P4-3149 misc 3rd party library bumps.
    
    * P4-3149 bumping to latest version of Flyway DB migration tool.
    
    * P4 3149 moving creation of session tables into the baseline SQL file. Upgrade to later version of Spring is conflicting with Flyway history table creation.
    
    * P4-3149 bumping to latest version of Gradle Wrapper.
    
    * P4-3149 major version bump for Thymeleaf dependency.
    
    * P4-3149 attempt at resolving out of memory error in build pipeline.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.4"
-  kotlin("plugin.spring") version "1.5.21"
-  kotlin("plugin.jpa") version "1.5.21"
-  kotlin("plugin.allopen") version "1.5.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.8"
+  kotlin("plugin.spring") version "1.5.30"
+  kotlin("plugin.jpa") version "1.5.30"
+  kotlin("plugin.allopen") version "1.5.30"
 }
 
 allOpen {
@@ -17,11 +17,11 @@ dependencyCheck {
 
 dependencies {
   implementation("com.beust:klaxon:5.5")
-  implementation("com.amazonaws:aws-java-sdk-s3:1.12.36")
-  implementation("io.sentry:sentry-spring-boot-starter:5.0.1")
-  implementation("net.javacrumbs.shedlock:shedlock-spring:4.25.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.25.0")
-  implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:2.5.3")
+  implementation("com.amazonaws:aws-java-sdk-s3:1.12.69")
+  implementation("io.sentry:sentry-spring-boot-starter:5.1.2")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:4.27.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.27.0")
+  implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.0.0")
   implementation("org.apache.poi:poi-ooxml:5.0.0")
 
   constraints {
@@ -36,13 +36,13 @@ dependencies {
     }
   }
 
-  implementation("org.flywaydb:flyway-core:7.12.0")
+  implementation("org.flywaydb:flyway-core:7.15.0")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.session:spring-session-jdbc:2.5.1")
+  implementation("org.springframework.session:spring-session-jdbc:2.5.2")
   implementation("org.springframework.shell:spring-shell-starter:2.0.1.RELEASE")
   implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5:3.0.4.RELEASE")
   implementation(kotlin("script-runtime"))
@@ -50,8 +50,7 @@ dependencies {
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.fluentlenium:fluentlenium-junit-jupiter:4.8.0")
   testImplementation("org.fluentlenium:fluentlenium-assertj:4.8.0")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1")
-  testImplementation("org.mockito:mockito-inline:3.11.2")
+  testImplementation("org.mockito:mockito-inline:3.12.4")
   testImplementation("org.seleniumhq.selenium:selenium-support:3.141.59")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.security:spring-security-test")
@@ -68,6 +67,11 @@ tasks {
   test {
     useJUnitPlatform()
     exclude("**/integration/*", "**/migrations/*")
+  }
+
+  withType<JavaCompile> {
+    sourceCompatibility = JavaVersion.VERSION_11.toString()
+    targetCompatibility = JavaVersion.VERSION_11.toString()
   }
 
   val testIntegration by registering(Test::class) {
