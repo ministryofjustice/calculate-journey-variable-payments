f5d06b6a2930f6796fd688090c5443645ddb4eb6
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2021-10-22 14:34:09 +0100

    P4-3267 this is test branch for uploading JSON files for historic move changes.

--- localstack/setup.sh
+++ localstack/setup.sh
@@ -11,40 +11,10 @@ aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/sche
 aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/geoamey_sample.xlsx s3://jpc/
 aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/serco_sample.xlsx s3://jpc/
 
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/01/2020-12-01-events.jsonl s3://basm/2020/12/01/2020-12-01-events.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/01/2020-12-01-journeys.jsonl s3://basm/2020/12/01/2020-12-01-journeys.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/01/2020-12-01-moves.jsonl s3://basm/2020/12/01/2020-12-01-moves.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/01/2020-12-01-people.jsonl s3://basm/2020/12/01/2020-12-01-people.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/01/2020-12-01-profiles.jsonl s3://basm/2020/12/01/2020-12-01-profiles.jsonl
+aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2021/10/21/2021-10-21-events.jsonl s3://basm/2021/10/21/2021-10-21-events.jsonl
+aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2021/10/21/2021-10-21-moves.jsonl s3://basm/2021/10/21/2021-10-21-moves.jsonl
 
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/02/2020-12-02-events.jsonl s3://basm/2020/12/02/2020-12-02-events.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/02/2020-12-02-journeys.jsonl s3://basm/2020/12/02/2020-12-02-journeys.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/02/2020-12-02-moves.jsonl s3://basm/2020/12/02/2020-12-02-moves.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/02/2020-12-02-people.jsonl s3://basm/2020/12/02/2020-12-02-people.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/02/2020-12-02-profiles.jsonl s3://basm/2020/12/02/2020-12-02-profiles.jsonl
-
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/03/2020-12-03-events.jsonl s3://basm/2020/12/03/2020-12-03-events.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/03/2020-12-03-journeys.jsonl s3://basm/2020/12/03/2020-12-03-journeys.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/03/2020-12-03-moves.jsonl s3://basm/2020/12/03/2020-12-03-moves.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/03/2020-12-03-people.jsonl s3://basm/2020/12/03/2020-12-03-people.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/03/2020-12-03-profiles.jsonl s3://basm/2020/12/03/2020-12-03-profiles.jsonl
-
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/04/2020-12-04-events.jsonl s3://basm/2020/12/04/2020-12-04-events.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/04/2020-12-04-journeys.jsonl s3://basm/2020/12/04/2020-12-04-journeys.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/04/2020-12-04-moves.jsonl s3://basm/2020/12/04/2020-12-04-moves.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/04/2020-12-04-people.jsonl s3://basm/2020/12/04/2020-12-04-people.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/04/2020-12-04-profiles.jsonl s3://basm/2020/12/04/2020-12-04-profiles.jsonl
-
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/05/2020-12-05-events.jsonl s3://basm/2020/12/05/2020-12-05-events.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/05/2020-12-05-journeys.jsonl s3://basm/2020/12/05/2020-12-05-journeys.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/05/2020-12-05-moves.jsonl s3://basm/2020/12/05/2020-12-05-moves.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/05/2020-12-05-people.jsonl s3://basm/2020/12/05/2020-12-05-people.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/05/2020-12-05-profiles.jsonl s3://basm/2020/12/05/2020-12-05-profiles.jsonl
-
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/06/2020-12-06-events.jsonl s3://basm/2020/12/06/2020-12-06-events.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/06/2020-12-06-journeys.jsonl s3://basm/2020/12/06/2020-12-06-journeys.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/06/2020-12-06-moves.jsonl s3://basm/2020/12/06/2020-12-06-moves.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/06/2020-12-06-people.jsonl s3://basm/2020/12/06/2020-12-06-people.jsonl
-aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2020/12/06/2020-12-06-profiles.jsonl s3://basm/2020/12/06/2020-12-06-profiles.jsonl
+aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2021/10/22/2021-10-22-events.jsonl s3://basm/2021/10/22/2021-10-22-events.jsonl
+aws --endpoint-url=http://localhost:4572 s3 cp /docker-entrypoint-initaws.d/2021/10/22/2021-10-22-moves.jsonl s3://basm/2021/10/22/2021-10-22-moves.jsonl
 
 echo "Buckets and seed data configured for schedule 34 locations and suppliers."
