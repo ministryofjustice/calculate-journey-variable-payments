e3223f3c91baaf2c0faf6c6cb645cd6cea479d09
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2022-03-22 14:32:24 +0000

    P4-3571 bumping 3rd party dependencies. (#451)
    * P4-3571 bumping 3rd party dependencies.
    
    * P4-3571 bumping to latest gradle wrapper.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.0"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
   kotlin("plugin.allopen") version "1.6.10"
@@ -16,14 +16,14 @@ dependencyCheck {
 }
 
 dependencies {
-  implementation("com.beust:klaxon:5.5")
-  implementation("com.amazonaws:aws-java-sdk-s3:1.12.165")
-  implementation("io.sentry:sentry-spring-boot-starter:5.6.1")
+  implementation("com.beust:klaxon:5.6")
+  implementation("com.amazonaws:aws-java-sdk-s3:1.12.181")
+  implementation("io.sentry:sentry-spring-boot-starter:5.7.0")
   implementation("net.javacrumbs.shedlock:shedlock-spring:4.33.0")
   implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.33.0")
-  implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.0.0")
-  implementation("org.apache.poi:poi-ooxml:5.2.0")
-  implementation("org.flywaydb:flyway-core:8.5.1")
+  implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.1.0")
+  implementation("org.apache.poi:poi-ooxml:5.2.2")
+  implementation("org.flywaydb:flyway-core:8.5.4")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
@@ -36,7 +36,7 @@ dependencies {
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.fluentlenium:fluentlenium-junit-jupiter:4.8.0")
   testImplementation("org.fluentlenium:fluentlenium-assertj:4.8.0")
-  testImplementation("org.mockito:mockito-inline:4.2.0")
+  testImplementation("org.mockito:mockito-inline:4.4.0")
   testImplementation("org.seleniumhq.selenium:selenium-java:4.1.2")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.security:spring-security-test")
