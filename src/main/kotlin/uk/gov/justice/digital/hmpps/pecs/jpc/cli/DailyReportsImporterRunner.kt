133546650fbadba6d6c646a9a65d6e21e8fd0995
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2022-07-21 15:54:25 +0100

    P4-3764 disable application task for testing of k8 replacemet task/job. (#487)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/cli/DailyReportsImporterRunner.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/cli/DailyReportsImporterRunner.kt
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.pecs.jpc.config.TimeSource
 import uk.gov.justice.digital.hmpps.pecs.jpc.service.reports.ImportReportsService
 
+/**
+ * When the application is started in non-web mode and the 'daily-reports' profile is used this runner will execute and
+ * import the daily reports for the previous day. Upon completion the application will exit.
+ */
 @ConditionalOnNotWebApplication
 @Component
 @Profile("daily-reports")
