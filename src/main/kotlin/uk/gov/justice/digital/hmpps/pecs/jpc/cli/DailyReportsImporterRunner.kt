b82197bec0621f30fc947968abbeac1cffb624d5
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2022-07-22 10:10:22 +0100

    P4-3764 disable application task for testing of k8 replacemet task/job in preprod and add support for back dating reports.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/cli/DailyReportsImporterRunner.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/cli/DailyReportsImporterRunner.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc.cli
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.pecs.jpc.config.TimeSource
 import uk.gov.justice.digital.hmpps.pecs.jpc.service.reports.ImportReportsService
+import java.time.LocalDate
 
 /**
  * When the application is started in non-web mode and the 'daily-reports' profile is used this runner will execute and
@@ -16,13 +18,28 @@ import uk.gov.justice.digital.hmpps.pecs.jpc.service.reports.ImportReportsServic
 @Component
 @Profile("daily-reports")
 class DailyReportsImporterRunner(
+  @Value("\${IMPORT_REPORTS_BACKDATE_ENABLED}") private val backDateEnabled: Boolean = false,
   private val service: ImportReportsService,
   private val timeSource: TimeSource,
 ) : ApplicationRunner {
 
   override fun run(arguments: ApplicationArguments) {
     timeSource.yesterday().run {
+      if (backDateEnabled) {
+        val startDate = service.dateOfLastImport() ?: this
+
+        if (startDate.isBefore(this)) {
+          backdateReportsFrom(startDate)
+
+          return
+        }
+      }
+
       service.importAllReportsOn(this)
     }
+  }
+
+  private fun backdateReportsFrom(from: LocalDate) {
+    from.datesUntil(timeSource.date()).forEach { service.importAllReportsOn(it) }
   }
 }
