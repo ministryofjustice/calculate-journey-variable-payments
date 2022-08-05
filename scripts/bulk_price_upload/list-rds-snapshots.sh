3038a31fb0bc4737b572bc8aa12ac6115200d874
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2022-08-05 14:20:38 +0100

    P4-3670 adding scripts to help with listing, taking and deleting snapshots. (#496)

---
+++ scripts/bulk_price_upload/list-rds-snapshots.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ENV=$1
+
+if [ -z "$ENV" ]
+then
+  echo "No environment specified, please supply environment in the first argument, dev, preprod or prod."
+else
+  NAMESPACE=calculate-journey-variable-payments-$ENV
+
+  echo using namespace "$NAMESPACE"
+
+  DATABASE=$(kubectl -n "$NAMESPACE" get secrets rds-instance-"$NAMESPACE" -o json | jq -r ".data | map_values(@base64d).database_host" | cut -d . -f 1)
+
+  echo using database instance ="$DATABASE"
+
+  ACCESS_KEY_ID=$(kubectl get secret rds-instance-"$NAMESPACE" -n calculate-journey-variable-payments-$ENV -o json | jq -r ".data | map_values(@base64d).access_key_id")
+  SECRET_ACCESS_KEY=$(kubectl get secret rds-instance-"$NAMESPACE" -n calculate-journey-variable-payments-$ENV -o json | jq -r ".data | map_values(@base64d).secret_access_key")
+
+  export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
+  export AWS_DEFAULT_REGION=eu-west-2
+
+  aws rds describe-db-snapshots --db-instance-identifier "$DATABASE"
+fi
