d99d006c17c841d4dab7dfed784cacd96830c033
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2022-08-15 15:40:18 +0100

    P4-3864 fixing sed command to run on mac and correcting cron time for job. (#506)

--- k8_jobs/apply-import-reports-job.sh
+++ k8_jobs/apply-import-reports-job.sh
@@ -3,9 +3,7 @@
 # This script creates a cron job for the daily reports ingress into the payment service.
 
 # The following command line tools are required to run this script:
-#  aws cli
 #  kubectl
-#  jq
 
 ENV=$1
 
@@ -14,8 +12,14 @@ then
   echo "No environment specified. Please supply required environment argument, dev, preprod or prod."
 else
   cp import-reports.yaml imports-reports-$ENV.yaml
-  sed -i 's/-placeholder/-'$ENV'/g' imports-reports-$ENV.yaml
-  kubectl config set-context --current --namespace=calculate-journey-variable-payments-$ENV
+
+  # sed command formatted for Mac OS (does not work on linux)
+  if ! sed -i '' 's/-placeholder/-'$ENV'/g' imports-reports-$ENV.yaml;
+  then
+    echo Attempting to running Linix sed
+    sed -i 's/-placeholder/-'$ENV'/g' imports-reports-$ENV.yaml
+  fi
+
   kubectl apply -f imports-reports-$ENV.yaml --namespace calculate-journey-variable-payments-$ENV
   rm imports-reports-$ENV.yaml
   kubectl get cronjobs -n calculate-journey-variable-payments-$ENV
