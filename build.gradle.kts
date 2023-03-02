2e6da3286c5a8bca22b9606ec6c34d919cb6af5c
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2023-03-02 11:56:14 +0000

    Bump DPS plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.4-beta-2"
   kotlin("plugin.spring") version "1.8.0"
   kotlin("plugin.jpa") version "1.8.0"
   kotlin("plugin.allopen") version "1.8.0"
