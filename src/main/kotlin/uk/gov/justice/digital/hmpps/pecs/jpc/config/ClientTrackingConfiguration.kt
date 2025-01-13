2365f3e33a04ed751cdebb09dcd811c61b421507
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2025-01-13 16:52:05 +0000

    MAP-2045 formatting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/config/ClientTrackingConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/config/ClientTrackingConfiguration.kt
@@ -2,15 +2,15 @@ package uk.gov.justice.digital.hmpps.pecs.jpc.config
 
 import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.jwt.SignedJWT
+import io.opentelemetry.api.trace.Span
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.slf4j.LoggerFactory
-import org.springframework.http.HttpHeaders
-import io.opentelemetry.api.trace.Span
 import java.text.ParseException
 
 @Configuration
