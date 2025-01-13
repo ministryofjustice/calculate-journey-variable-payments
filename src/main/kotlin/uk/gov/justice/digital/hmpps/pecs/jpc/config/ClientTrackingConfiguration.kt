075774165762943cacc43357c2524721795bab05
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2025-01-13 16:41:52 +0000

    MAP-2045 adding missing indexes and azue logs configuration

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/config/ClientTrackingConfiguration.kt
@@ -0,0 +1,56 @@
+package uk.gov.justice.digital.hmpps.pecs.jpc.config
+
+import com.nimbusds.jwt.JWTClaimsSet
+import com.nimbusds.jwt.SignedJWT
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
+import io.opentelemetry.api.trace.Span
+import java.text.ParseException
+
+@Configuration
+class ClientTrackingConfiguration(private val clientTrackingInterceptor: ClientTrackingInterceptor) : WebMvcConfigurer {
+  override fun addInterceptors(registry: InterceptorRegistry) {
+    log.info("Adding application insights client tracking interceptor")
+    registry.addInterceptor(clientTrackingInterceptor).addPathPatterns("/**")
+  }
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+}
+
+@Configuration
+class ClientTrackingInterceptor : HandlerInterceptor {
+  override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+    val token = request.getHeader(HttpHeaders.AUTHORIZATION)
+    if (token?.startsWith("Bearer ") == true) {
+      try {
+        val jwtBody = getClaimsFromJWT(token)
+        val user = jwtBody.getClaim("user_name")?.toString()
+        val currentSpan = Span.current()
+        user?.run {
+          currentSpan.setAttribute("username", this) // username in customDimensions
+          currentSpan.setAttribute("enduser.id", this) // user_Id at the top level of the request
+        }
+        currentSpan.setAttribute("clientId", jwtBody.getClaim("client_id").toString())
+      } catch (e: ParseException) {
+        log.warn("problem decoding jwt public key for application insights", e)
+      }
+    }
+    return true
+  }
+
+  @Throws(ParseException::class)
+  private fun getClaimsFromJWT(token: String): JWTClaimsSet =
+    SignedJWT.parse(token.replace("Bearer ", "")).jwtClaimsSet
+
+  companion object {
+    private val log = LoggerFactory.getLogger(ClientTrackingInterceptor::class.java)
+  }
+}
---
+++ src/main/resources/db/migration/ddl/V1_22__add_missing_indexes.sql
@@ -0,0 +1,4 @@
+create index if not exists move_type_idx on moves(move_type);
+create index if not exists supplier_idx on moves(supplier);
+create index if not exists drop_off_or_cancelled_idx on moves(drop_off_or_cancelled);
+
