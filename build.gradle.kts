23fe7bf03a76ab431c74c2fd87c9243572589217
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2025-01-27 09:27:18 +0000

    MAP-2087 security update (#580)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.2"
   kotlin("plugin.spring") version "2.0.21"
   kotlin("plugin.jpa") version "2.0.21"
   kotlin("plugin.allopen") version "2.0.21"
