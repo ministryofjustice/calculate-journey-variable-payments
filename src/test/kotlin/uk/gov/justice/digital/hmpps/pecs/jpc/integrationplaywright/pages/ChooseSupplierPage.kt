896693e8c0c3e36637a9195ec82acb1fc1dae679
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2025-03-18 13:57:47 +0000

    MAP-2277 replacing GotoDashboardAndDownloadSpreadsheetTest (#586)
    * MAP-2277 replacing GotoDashboardAndDownloadSpreadsheetTest

--- src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/integrationplaywright/pages/ChooseSupplierPage.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/integrationplaywright/pages/ChooseSupplierPage.kt
@@ -1,13 +1,19 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc.integrationplaywright.pages
 
 import com.microsoft.playwright.Page
+import uk.gov.justice.digital.hmpps.pecs.jpc.domain.price.Supplier
 
 class ChooseSupplierPage(page: Page?) {
 
   private val url = "http://localhost:8080/choose-supplier"
   private val page = page
+
   fun gotToPage() {
     page?.navigate(url)
+  }
+
+  fun gotToPage(supplier: Supplier) {
+    page?.navigate("$url/${supplier.name.lowercase()}")
   }
 
   fun isPageSuccessful(): Boolean {
--- src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/integrationplaywright/pages/DashboardPage.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/integrationplaywright/pages/DashboardPage.kt
@@ -5,16 +5,16 @@ import uk.gov.justice.digital.hmpps.pecs.jpc.domain.price.Supplier
 
 class DashboardPage(page: Page?) {
 
-  private val chooseSupplierUrl = "http://localhost:8080/choose-supplier"
   private val dashboardUrl = "http://localhost:8080/dashboard"
   private val page = page
-
-  fun gotToPage(supplier: Supplier) {
-    page?.navigate("$chooseSupplierUrl/${supplier.name.lowercase()}")
-  }
 
   fun isPageSuccessful(supplier: Supplier): Boolean {
     page?.waitForURL(dashboardUrl)
     return page?.url().equals(dashboardUrl) && page?.getByText(supplier.name)?.first()?.isVisible == true
+  }
+
+  fun isDownloadAllMovesActive(): Boolean {
+    val download = page?.waitForDownload { page.locator("a.download-icon").click() }
+    return download?.suggestedFilename()?.endsWith(".xlsx") == true
   }
 }
