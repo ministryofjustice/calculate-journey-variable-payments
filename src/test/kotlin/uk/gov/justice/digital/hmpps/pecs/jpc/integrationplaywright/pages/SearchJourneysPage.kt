24249b465179595047d7013bc24eb4cfe52bd069
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2025-04-08 12:37:53 +0100

    MAP-2280 test replacement

--- src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/integrationplaywright/pages/SearchJourneysPage.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/integrationplaywright/pages/SearchJourneysPage.kt
@@ -1,8 +1,11 @@
 package uk.gov.justice.digital.hmpps.pecs.jpc.integrationplaywright.pages
 
 import com.microsoft.playwright.Page
+import com.microsoft.playwright.PlaywrightException
 import com.microsoft.playwright.options.AriaRole
+import com.microsoft.playwright.options.WaitForSelectorState
 import uk.gov.justice.digital.hmpps.pecs.jpc.util.loggerFor
+import java.time.LocalDate
 
 class SearchJourneysPage(page: Page?) {
 
@@ -20,14 +23,22 @@ class SearchJourneysPage(page: Page?) {
   }
 
   fun findJourney(fromAgency: String, toAgency: String): Boolean {
+    page?.waitForLoadState()
     page?.locator("input#from")?.fill(fromAgency)
-    page?.waitForSelector("//ul[@id='from__listbox']/li", Page.WaitForSelectorOptions().setTimeout(1000.0))
-    page?.locator("//ul[@id='from__listbox']/li[1]")?.click()
+    try {
+      page?.waitForSelector("#from__listbox li", Page.WaitForSelectorOptions().setTimeout(2000.0))
+    } catch (e: PlaywrightException) {
+      return false
+    }?.click()
     page?.locator("input#to")?.fill(toAgency)
-    page?.waitForSelector("//ul[@id='to__listbox']/li", Page.WaitForSelectorOptions().setTimeout(1000.0))
-    page?.locator("//ul[@id='to__listbox']/li[1]")?.click()
+    try {
+      page?.waitForSelector("#to__listbox li", Page.WaitForSelectorOptions().setTimeout(2000.0))
+    } catch (e: PlaywrightException) {
+      return false
+    }?.click()
     page?.getByRole(AriaRole.BUTTON, Page.GetByRoleOptions().setName("Find Journeys"))?.click()
     return page?.waitForSelector("h1")?.innerText()
       ?.startsWith("Manage Journey Price Catalogue") == true
   }
+
 }
