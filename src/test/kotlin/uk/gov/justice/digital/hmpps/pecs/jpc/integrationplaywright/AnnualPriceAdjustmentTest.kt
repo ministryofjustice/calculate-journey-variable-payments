7a42a87ccd7f5e5cc96b803d7c92dcdc6f606468
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2025-05-14 10:28:42 +0100

    MAP-2283 (#593)
    * MAP-2283 removing selenium tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/integrationplaywright/AnnualPriceAdjustmentTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pecs/jpc/integrationplaywright/AnnualPriceAdjustmentTest.kt
@@ -11,7 +11,7 @@ import kotlin.random.Random
 
 internal class AnnualPriceAdjustmentTest : PlayWrightTest() {
 
-  // @Test
+  @Test
   fun `Apply Inflationary rate price adjustments for SERCO`() {
     val loginPage = LoginPage(page)
     val supplierPage = ChooseSupplierPage(page)
@@ -27,7 +27,7 @@ internal class AnnualPriceAdjustmentTest : PlayWrightTest() {
     manageJourneyPriceCatalogue.gotToPage()
     assert(manageJourneyPriceCatalogue.isPageSuccessful())
     manageJourneyPriceCatalogue.goToAnnualPriceAdjustment()
-    val rate = String.format("%.12f", Random.nextDouble(0.01, 0.99))
+    val rate = String.format("%.2f", Random.nextDouble(0.01, 0.99))
     val details = "Inflationary rate of $rate."
     val formSubmitted = annualPriceAdjustmentPage.applyBulkPriceAdjustment(rate, null, details)
     assert(formSubmitted) { "Validation error occurred for $details" }
@@ -35,7 +35,7 @@ internal class AnnualPriceAdjustmentTest : PlayWrightTest() {
     assert(annualPriceAdjustmentPage.isPriceAdjustmentRecordsPresent(details))
   }
 
-  // @Test
+  @Test
   fun `Apply Inflationary and Volumetric rate price adjustments for SERCO`() {
     val loginPage = LoginPage(page)
     val supplierPage = ChooseSupplierPage(page)
@@ -51,8 +51,8 @@ internal class AnnualPriceAdjustmentTest : PlayWrightTest() {
     manageJourneyPriceCatalogue.gotToPage()
     assert(manageJourneyPriceCatalogue.isPageSuccessful())
     manageJourneyPriceCatalogue.goToAnnualPriceAdjustment()
-    val inflationaryRate = String.format("%.12f", Random.nextDouble(0.01, 0.99))
-    val volumetricRate = String.format("%.12f", Random.nextDouble(1.0, 9.0))
+    val inflationaryRate = String.format("%.2f", Random.nextDouble(0.01, 0.99))
+    val volumetricRate = String.format("%.2f", Random.nextDouble(1.0, 9.0))
     val details = "Inflationary rate of $inflationaryRate and Volumetric rate of $volumetricRate."
     val formSubmitted = annualPriceAdjustmentPage.applyBulkPriceAdjustment(inflationaryRate, volumetricRate, details)
     assert(formSubmitted) { "Validation error occurred for $details" }
@@ -60,7 +60,7 @@ internal class AnnualPriceAdjustmentTest : PlayWrightTest() {
     assert(annualPriceAdjustmentPage.isPriceAdjustmentRecordsPresent(details))
   }
 
-  // @Test
+  @Test
   fun `Apply Inflationary rate price adjustments for GEOAMEY`() {
     val loginPage = LoginPage(page)
     val supplierPage = ChooseSupplierPage(page)
@@ -76,7 +76,7 @@ internal class AnnualPriceAdjustmentTest : PlayWrightTest() {
     manageJourneyPriceCatalogue.gotToPage()
     assert(manageJourneyPriceCatalogue.isPageSuccessful())
     manageJourneyPriceCatalogue.goToAnnualPriceAdjustment()
-    val rate = String.format("%.12f", Random.nextDouble(0.01, 0.99))
+    val rate = String.format("%.2f", Random.nextDouble(0.01, 0.99))
     val details = "Inflationary rate of $rate."
     val formSubmitted = annualPriceAdjustmentPage.applyBulkPriceAdjustment(rate, null, details)
     assert(formSubmitted) { "Validation error occurred for $details" }
@@ -100,8 +100,8 @@ internal class AnnualPriceAdjustmentTest : PlayWrightTest() {
     manageJourneyPriceCatalogue.gotToPage()
     assert(manageJourneyPriceCatalogue.isPageSuccessful())
     manageJourneyPriceCatalogue.goToAnnualPriceAdjustment()
-    val inflationaryRate = String.format("%.12f", Random.nextDouble(0.01, 0.99))
-    val volumetricRate = String.format("%.12f", Random.nextDouble(1.0, 9.0))
+    val inflationaryRate = String.format("%.2f", Random.nextDouble(0.01, 0.99))
+    val volumetricRate = String.format("%.2f", Random.nextDouble(1.0, 9.0))
     val details = "Inflationary rate of $inflationaryRate and Volumetric rate of $volumetricRate."
     val formSubmitted = annualPriceAdjustmentPage.applyBulkPriceAdjustment(inflationaryRate, volumetricRate, details)
     assert(formSubmitted) { "Validation error occurred for $details" }
